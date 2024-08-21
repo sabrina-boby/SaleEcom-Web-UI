71450079409f96c4cf86ec78685e7db4d8b13eb5
Ejaz0000 <ejahmed999@gmail.com>
2024-08-21 17:20:17 +0600

    count updated

--- src/app/pages/home/business-section/business-section.component.ts
+++ src/app/pages/home/business-section/business-section.component.ts
@@ -1,20 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-business-section',
   templateUrl: './business-section.component.html',
   styleUrl: './business-section.component.scss'
 })
-export class BusinessSectionComponent {
-   count01 : number = 0;
+export class BusinessSectionComponent implements OnInit{
+  
+  count01: number = 500;
+  count02: number = 0;
+  count03: number = 0;
+
+  isVisible: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(): void {
+    const element = document.getElementById('bs');
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const topInView = rect.top >= 0 && rect.top <= window.innerHeight;
+
+      if (topInView && !this.isVisible) {
+        this.isVisible = true;
+        this.startCounter01(680);
+        this.startCounter02(20);
+        this.startCounter03(50);
+
+      }
+    }
+  }
 
 
-  // counterstop:any=setInterval(()=>{
-  //   this.count01++;
+  startCounter01(stopNumber:number): void {
+    const interval = setInterval(() => {
+      if (this.count01 < stopNumber) {
+        this.count01++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 20); 
+  }
 
-  //   if(this.count01 === 680){
-  //     clearInterval(this.counterstop);
-  //   }
-  // },50)
+  startCounter02(stopNumber:number): void {
+    const interval = setInterval(() => {
+      if (this.count02 < stopNumber) {
+        this.count02++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 80); 
+  }
+
+  startCounter03(stopNumber:number): void {
+    const interval = setInterval(() => {
+      if (this.count03 < stopNumber) {
+        this.count03++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 60); 
+  }
 
 }
