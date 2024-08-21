bd756a614c7d8d1701e8deda28991bcf5da23976
Ejaz0000 <ejahmed999@gmail.com>
2024-08-21 15:21:27 +0600

    updated

--- src/app/pages/home/business-section/business-section.component.ts
+++ src/app/pages/home/business-section/business-section.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './business-section.component.scss'
 })
 export class BusinessSectionComponent {
+   count01 : number = 680;
 
+
+  //  async function counter(){
+  //   for (let i: number = 0; i < 680; i++) {
+  //     await delay(1000);
+  //     this.count01++;
+  // }
+  //  }
 }
